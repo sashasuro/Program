e4b3c235c6b79c408e7034dfac28c3a1a653be3e
sashasuro <surovskaasasa@gmail.com>
2023-01-31 21:59:34 +0300

    Create Program1.cs
    Program1.cs

---
+++ Program1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycles2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Task 1
+
+                //Левый верхний
+
+                //for(int i = 0; i < 20; i++)
+                //{
+                //    Console.Write('#');
+                //}
+                //Console.WriteLine();
+                //for(int i = 0; i < 20; i++)
+                //{
+                //    Console.WriteLine('#');
+                //}
+                //Console.ReadKey();
+
+
+                //Левый нижний 
+
+                //int i =0;
+                //int j = 0;
+                //int a;
+                //for ( ; i < 20; i++)
+                //{
+                //    Console.WriteLine('#');
+                //    if (i == 19)
+                //    {
+                //        a = i+1;
+                //    }
+                //}
+                //Console.SetCursorPosition(0, i);
+                //for ( ; j < 20; j++)
+                //{
+                //    Console.Write('#');
+                //}
+                //Console.ReadKey();
+
+
+                //for (int i = 0; i < 20; i++)
+                //{
+                //    Console.WriteLine('#');
+                //}
+                //for (int i = 0; i < 20; i++)
+                //{
+                //    Console.Write('#');
+                //}
+                //Console.ReadKey();
+
+
+                //Правый верхний 
+
+                //int i =0;
+                //int j = 0;
+                //for ( ; i < 20; i++)
+                //{
+                //    Console.Write('#');
+                //}
+                //for ( ; j < 20; j++)
+                //{
+
+                //    Console.SetCursorPosition(i, j);
+                //    Console.WriteLine('#');
+                //}
+                //Console.ReadKey();
+
+
+                //Правый нижний 
+
+                //int i = 0;
+                //int j = 0;
+                //int k = 0;
+                //for (; k < 19; k++)
+                //{
+                //    Console.Write("");
+                //}
+                //for (; j < 20; j++)
+                //{
+                //    Console.Write('#');
+                //    Console.SetCursorPosition(j, i);
+                //}
+                //for (; i < 20; i++)
+                //{
+
+                //    Console.SetCursorPosition(k, i);
+                //    Console.WriteLine('#');
+                //}
+                //Console.ReadKey();
+
+
+                //Task 2
+
+                //Полный квадрат
+
+                //int i = 0;
+                //int j = 0;
+                //int k = 0;
+                //int o = 0;
+                //for(; k < 20; k++)
+                //{
+                //    Console.Write('#');
+                //}
+                //for (; j < 20; j++)
+                //{
+                //    Console.WriteLine('#');
+                //}
+                //for (; i < 20; i++)
+                //{
+
+                //    Console.SetCursorPosition(j, i);
+                //    Console.Write('#');
+                //}
+
+                //Console.SetCursorPosition(0, j);
+                //for (; o < 21; o++)
+                //{
+                //    Console.Write('#');
+                //}
+                //Console.ReadKey();
+
+
+                //Task 3
+
+                //for (int i = 0; i < 5; i++)
+                //{
+                //    for (int j = 0; j < 5 - i; j++)
+                //        Console.Write("{0} ", j);
+                //    Console.WriteLine();
+                //}
+                //Console.ReadKey();
+
+
+                //Task 4
+
+                //for(int i = 0; i < 5; i++)
+                //{
+                //    for(int j=0;j<5;j++)
+                //    {
+                //        Console.Write("2   ");
+                //    }
+                //    Console.WriteLine();
+                //}
+                //Console.ReadKey();
+
+            }
+            
+
+        }
+    }
+}
